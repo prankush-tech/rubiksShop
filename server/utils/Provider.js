672f00df4f7a2b18ff317acda788502a85b2ac95
Prankush Giri <76916192+prankush-tech@users.noreply.github.com>
2023-02-17 17:08:46 +0530

    🦄test01

--- server/utils/Provider.js
+++ server/utils/Provider.js
@@ -9,7 +9,6 @@ export const connectPassport = () => {
 				clientID: process.env.GOOGLE_CLIENT_ID,
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 				callbackURL: process.env.GOOGLE_CALLBACK_URL,
-				passReqToCallback: true
 			},
 			async (request, accessToken, refreshToken, profile, done) => {
 				//datbase
