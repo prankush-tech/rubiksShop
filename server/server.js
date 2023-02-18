8299664a6e3365e2a27ac9fb8d6ad21f2777cb21
Prankush Giri <76916192+prankush-tech@users.noreply.github.com>
2023-02-18 23:19:48 +0530

    🎁LetsSee🎁

--- server/server.js
+++ server/server.js
@@ -10,6 +10,11 @@ connectDB()
 app.get('/',(req,res)=>{
     res.send("<h1>working</h1>");
 })
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "http://localhost:5173");
+    next()
+  });
 
 app.listen(process.env.PORT,()=>{
     console.log(`http://localhost:${process.env.PORT} in ${process.env.NODE_ENV} mode`);
