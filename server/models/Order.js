f12a7d577ddfc3324dac21a8378d15dcf04f2d95
Prankush Giri <76916192+prankush-tech@users.noreply.github.com>
2023-02-19 23:33:36 +0530

    ✅JAI SHREE RAM🍟

--- server/models/Order.js
+++ server/models/Order.js
@@ -19,7 +19,7 @@ const schema = new mongoose.Schema({
             type:String,
             required:true,
         },
-        pincode:{
+        pinCode:{
             type:Number,
             required:true,
         },
